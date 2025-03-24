b0613db5c3d6325bfaf50ddd476f144cbf740148
SmallGuillotine <foodpunch@gmail.com>
2025-03-25 01:04:44 +0800

    Changes

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,9 +1,9 @@
-// src/PrivateRoute.js
-import React from 'react';
-import { withAuthenticationRequired } from "react-oidc-context";
+// // src/PrivateRoute.js
+// import React from 'react';
+// import { withAuthenticationRequired } from "react-oidc-context";
 
-const PrivateRoute = () => (<div>Private</div>);
+// const PrivateRoute = () => (<div>Private</div>);
 
-export default withAuthenticationRequired(PrivateRoute, {
-  OnRedirecting: () => (<div>Redirecting to the login page...</div>)
-});
+// export default withAuthenticationRequired(PrivateRoute, {
+//   OnRedirecting: () => (<div>Redirecting to the login page...</div>)
+// });
